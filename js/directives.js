3762d53a2955953ba1e373f1077fa07d2db74d9b
Venkata Giri Reddy <venkatagirireddy@gmail.com>
2013-10-08 23:48:19 +0400

    Use angular.forEach for looping.

--- js/directives.js
+++ js/directives.js
@@ -5,14 +5,13 @@ angular
   return {
     restrict: "A",
     link: function(scope, element, attrs, controller) {
-      var klass = attrs.activeLink,
-        links = element[0].getElementsByTagName("a");
+      var klass = attrs.activeLink;
       scope.location = $location;
       scope.$watch("location.path()", function(newPath) {
-        for(var i=0, len=links.length; i < len; i++) {
-          if(links[i].hash.substring(1) == newPath) links[i].classList.add(klass);
-          else links[i].classList.remove(klass);
-        }
+        angular.forEach(element[0].getElementsByTagName("a"), function(link) {
+          if(link.hash.substring(1) == newPath) link.classList.add(klass);
+          else link.classList.remove(klass);
+        });
       });
     }
   };
